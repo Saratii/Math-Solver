0f7b55414a8edf74af818a5af684371c4a12a9f4
Sarati <friedchicken14821@gmail.com>
2022-05-21 01:41:02 -0500

    added multiply and divide

--- src/app/Nodes.ts
+++ src/app/Nodes.ts
@@ -1,21 +1,70 @@
 export abstract class MyNode{
-    abstract evaluate(): number;
+    abstract evaluate(): number | undefined;
+    abstract priority: number;
+    abstract display(): string;
 }
 export abstract class BinaryOperationNode extends MyNode{
     left!: MyNode;
     right!: MyNode;
 }
 export class PlusNode extends BinaryOperationNode{
-    evaluate(): number {
-        return this.left.evaluate() + this.right.evaluate();
+    priority = 1;
+    evaluate(): number | undefined{
+        let left = this.left.evaluate();
+        let right = this.right.evaluate();
+        if(left === undefined || right === undefined){
+            return undefined;
+        }
+        return left + right;
+    }
+    display(): string{
+        return "{+ " + this.left.display() + ", " + this.right.display() + "}";
     }
 }
 export class MinusNode extends BinaryOperationNode{
-    evaluate(): number {
-        return parseFloat((this.left.evaluate() - this.right.evaluate()).toFixed(16));
+    priority = 1;
+    evaluate(): number | undefined{
+        let left = this.left.evaluate();
+        let right = this.right.evaluate();
+        if(left === undefined || right === undefined){
+            return undefined;
+        }
+        return left-right;
+    }
+    display(): string{
+        return "{- " + this.left.display() + ", " + this.right.display() + "}";
+    }
+}
+export class MultiplyNode extends BinaryOperationNode{
+    priority = 2;
+    evaluate(): number | undefined{
+        let left = this.left.evaluate();
+        let right = this.right.evaluate();
+        if(left === undefined || right === undefined){
+            return undefined;
+        }
+        return left * right;
+    }
+    display(): string{
+        return "{* " + this.left.display() + ", " + this.right.display() + "}";
+    }
+}
+export class DivisionNode extends BinaryOperationNode{
+    priority = 2;
+    evaluate(): number | undefined{
+        let right = this.right.evaluate();
+        let left = this.left.evaluate();
+        if(right === 0 || right === undefined || left === undefined){
+            return undefined;
+        }
+        return left / right;
+    }
+    display(): string{
+        return "{/ " + this.left.display() + ", " + this.right.display() + "}";
     }
 }
 export class NumberNode extends MyNode{
+    priority = 69;
     val: number;
     constructor(val:number){
         super();
@@ -23,5 +72,8 @@ export class NumberNode extends MyNode{
     }
     evaluate(): number{
         return this.val;
+    }
+    display(): string{
+        return this.val + "";
     }
 }
