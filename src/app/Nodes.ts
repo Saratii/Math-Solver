bc78edc667d28f67191a2f523020c193bef01869
Sarati <friedchicken14821@gmail.com>
2022-05-25 01:59:34 -0500

    added negative numbers and testing system

--- src/app/Nodes.ts
+++ src/app/Nodes.ts
@@ -4,63 +4,69 @@ export abstract class MyNode{
     abstract display(): string;
 }
 export abstract class BinaryOperationNode extends MyNode{
-    left!: MyNode;
-    right!: MyNode;
+    left?: MyNode;
+    right?: MyNode;
+    public constructor(left:MyNode | undefined, right:MyNode | undefined){
+        super();
+        this.left = left;
+        this.right = right;
+    }
 }
 export class PlusNode extends BinaryOperationNode{
     priority = 1;
     evaluate(): number | undefined{
-        let left = this.left.evaluate();
-        let right = this.right.evaluate();
+        let left = this.left?.evaluate();
+        let right = this.right?.evaluate();
         if(left === undefined || right === undefined){
             return undefined;
         }
         return left + right;
     }
     display(): string{
-        return "{+ " + this.left.display() + ", " + this.right.display() + "}";
+        return "{" + this.left?.display() + " + " + this.right?.display() + "}";
     }
 }
 export class MinusNode extends BinaryOperationNode{
     priority = 1;
     evaluate(): number | undefined{
-        let left = this.left.evaluate();
-        let right = this.right.evaluate();
+        let left = this.left?.evaluate();
+        let right = this.right?.evaluate();
         if(left === undefined || right === undefined){
             return undefined;
         }
         return left-right;
     }
     display(): string{
-        return "{- " + this.left.display() + ", " + this.right.display() + "}";
+        return "{" + this.left?.display() + " - " + this.right?.display() + "}";
     }
+    
 }
 export class MultiplyNode extends BinaryOperationNode{
     priority = 2;
     evaluate(): number | undefined{
-        let left = this.left.evaluate();
-        let right = this.right.evaluate();
+        let left = this.left?.evaluate();
+        let right = this.right?.evaluate();
         if(left === undefined || right === undefined){
             return undefined;
         }
         return left * right;
     }
     display(): string{
-        return "{* " + this.left.display() + ", " + this.right.display() + "}";
+        return "{" + this.left?.display() + " * " + this.right?.display() + "}";
     }
 }
 export class DivisionNode extends BinaryOperationNode{
     priority = 2;
     evaluate(): number | undefined{
-        let right = this.right.evaluate();
-        let left = this.left.evaluate();
+        let right = this.right?.evaluate();
+        let left = this.left?.evaluate();
         if(right === 0 || right === undefined || left === undefined){
             return undefined;
         }
         return left / right;
     }
     display(): string{
-        return "{/ " + this.left.display() + ", " + this.right.display() + "}";
+        return "{" + this.left?.display() + " / " + this.right?.display() + "}";
     }
 }
 export class NumberNode extends MyNode{
@@ -75,5 +81,25 @@ export class NumberNode extends MyNode{
     }
     display(): string{
         return this.val + "";
+    }
+}
+
+export abstract class UnaryOperationNode{
+    child?: MyNode;
+    constructor(child:MyNode | undefined){
+        this.child = child;
+    }
+}
+export class NegativeNode extends UnaryOperationNode{
+    priority = 69;
+    evaluate(): number | undefined{
+        let child = this.child?.evaluate();
+        if(child === undefined){
+            return undefined;
+        }
+        return child * -1; 
+    }
+    display(): string{
+        return "neg(" + this.child?.display() + ")";
     }
 }
--- src/app/token.service.ts
+++ src/app/token.service.ts
@@ -1,7 +1,7 @@
-import { Binary, IfStmt } from '@angular/compiler';
+import { Binary, IfStmt, UnaryOperator } from '@angular/compiler';
 import { Injectable } from '@angular/core';
-import { BinaryOperationNode, DivisionNode, MinusNode, MultiplyNode, MyNode, NumberNode, PlusNode } from './Nodes';
-import { DivisionToken, MinusToken, MultiplyToken, NumberToken, PlusToken, Token } from './Tokens';
+import { BinaryOperationNode, DivisionNode, MinusNode, MultiplyNode, MyNode, NegativeNode, NumberNode, PlusNode, UnaryOperationNode } from './Nodes';
+import { DivisionToken, MinusToken, MultiplyToken, NegativeToken, NumberToken, PlusToken, Token } from './Tokens';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +12,14 @@ export class TokenService {
         let tokenlist = [];
         while(s){
             if(/^\-/.test(s)){
-                tokenlist.push(new MinusToken());
+                if(tokenlist.length>0 && tokenlist[tokenlist.length-1] instanceof NumberToken){
+                    tokenlist.push(new MinusToken());
+                } else {
+                    tokenlist.push(new NegativeToken());
+                }
                 s = s.substring(1);
             } else if(/^\-?[0-9]+(\.[0-9]+)?/.test(s)){
                 let nextNum = (s.match(/^\-?[0-9]+(\.[0-9]+)?/)||[])[0];
-                let results = (s.match(/^\-?[0-9]+(\.[0-9]+)?/));
-                console.log(results);
                 tokenlist.push(new NumberToken(parseFloat(nextNum)));
                 s = s.substring(nextNum.length);
             } else if(/^\+/.test(s)){
@@ -41,39 +43,48 @@ export class TokenService {
         let unusedNodes: MyNode[] = []; //makes unused node list
         for(let token of tokens){ //loops through tokens
             if(token as any instanceof NumberToken){ //if token is a number
-                if(unusedNodes.length === 0){ //if the unused node list is empty
-                    unusedNodes.push(new NumberNode((token as NumberToken).val)); //adds number node to unused nodes
-                } else if(unusedNodes.length > 1){  //if there is already a node in there
-                    let operationNode: BinaryOperationNode = unusedNodes.pop() as BinaryOperationNode; //take the last node added (operation node)
-                    let leftNode = unusedNodes.pop() as MyNode; //make the number in there as the left child of the operator
-                    let rightNode = new NumberNode((token as NumberToken).val);
-                    if(leftNode.priority < operationNode.priority){
-                        let leftRightNode = (leftNode as BinaryOperationNode).right;
-                        operationNode.left = leftRightNode;
-                        operationNode.right = rightNode;
-                        (leftNode as BinaryOperationNode).right = this.orderSwap(operationNode);
-                        unusedNodes.push(leftNode);
-                    } else {
-                        operationNode.left = leftNode;
-                        operationNode.right = rightNode;
-                        unusedNodes.push(operationNode);
-                    }
-                }
+                this.combineNodes(unusedNodes, new NumberNode((token as NumberToken).val));
+                // if(unusedNodes.length === 0){ //if the unused node list is empty
+                //     unusedNodes.push(new NumberNode((token as NumberToken).val)); //adds number node to unused nodes
+                // } else if(unusedNodes.length > 1){  //if there is already a node in there
+                //     let previousNode = unusedNodes.pop();
+                //     if(previousNode instanceof BinaryOperationNode){
+                //         let operationNode: BinaryOperationNode = previousNode as BinaryOperationNode; //take the last node added (operation node)
+                //         let leftNode = unusedNodes.pop() as MyNode; //make the number in there as the left child of the operator
+                //         let rightNode = new NumberNode((token as NumberToken).val);
+                //         if(leftNode.priority < operationNode.priority){
+                //             let leftRightNode = (leftNode as BinaryOperationNode).right;
+                //             operationNode.left = leftRightNode;
+                //             operationNode.right = rightNode;
+                //             (leftNode as BinaryOperationNode).right = this.orderSwap(operationNode);
+                //             unusedNodes.push(leftNode);
+                //         } else {
+                //             operationNode.left = leftNode;
+                //             operationNode.right = rightNode;
+                //             unusedNodes.push(operationNode);
+                //         }
+                //     } else if(previousNode instanceof UnaryOperationNode){
+                //         previousNode.child = new NumberNode((token as NumberToken).val);
+                //         unusedNodes.push(previousNode);
+                //     } 
+                // }
             } else if(token as any instanceof MinusToken){
-                unusedNodes.push(new MinusNode());
+                unusedNodes.push(new MinusNode(undefined, undefined));
             } else if(token as any instanceof PlusToken){
-                unusedNodes.push(new PlusNode());
+                unusedNodes.push(new PlusNode(undefined, undefined));
             } else if(token as any instanceof MultiplyToken){
-                unusedNodes.push(new MultiplyNode());
+                unusedNodes.push(new MultiplyNode(undefined, undefined));
             } else if(token as any instanceof DivisionToken){
-                unusedNodes.push(new DivisionNode());
+                unusedNodes.push(new DivisionNode(undefined, undefined));
+            } else if(token as any instanceof NegativeToken){
+                unusedNodes.push(new NegativeNode(undefined));
             }
         }
         return unusedNodes.pop() || new NumberNode(69);
     }
 
     orderSwap(node:BinaryOperationNode){
-        if(node.priority>node.right.priority){
+        if(node.priority>(node?.right?.priority || 69)){
             let operationNode = node.right as BinaryOperationNode;
             let leftNode = node.left;
             let operationNodeLeft = operationNode.left;
@@ -86,6 +97,34 @@ export class TokenService {
             return operationNode;
         } else {
             return node;
+        }
+    }
+    combineNodes(unusedNodes:MyNode[], latestNode:MyNode){
+        if(unusedNodes.length === 0){
+            unusedNodes.push(latestNode);
+            return;
+        }
+        let topNode = unusedNodes.pop();
+        if(topNode instanceof UnaryOperationNode){
+            topNode.child = latestNode;
+            this.combineNodes(unusedNodes, topNode);
+        } else if(topNode instanceof BinaryOperationNode){
+            if(unusedNodes.length === 0){
+                unusedNodes.push(topNode);
+                return;
+            }
+            let leftNode:MyNode = unusedNodes.pop() as MyNode;
+            if(leftNode.priority < topNode.priority){
+                let leftRightNode = (leftNode as BinaryOperationNode).right;
+                topNode.left = leftRightNode;
+                topNode.right = latestNode;
+                (leftNode as BinaryOperationNode).right = this.orderSwap(topNode);
+                unusedNodes.push(leftNode);
+            } else {
+                topNode.left = leftNode;
+                topNode.right = latestNode;
+                unusedNodes.push(topNode);
+            }
         }
     }
 }
